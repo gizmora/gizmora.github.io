907c56d34e069bfb6ffefa62ae8a2cc6e88d7519
Grace Mora <50357533+gizmora@users.noreply.github.com>
2024-12-17 23:47:20 +0800

    chore: cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 // src/index.js
 import './style.scss';
-
-const heading = document.createElement('h1');
-heading.textContent = 'Welcome to my Portfolio!';
-document.getElementById('root').appendChild(heading);
