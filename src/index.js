a3d17c85599c2ee7c96a55734c09e45836d7d8a9
Grace Mora <50357533+gizmora@users.noreply.github.com>
2025-01-11 00:18:43 +0800

    chore: dynamic work exp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.scss';
 import sections from './data/tech.json';
+import experiences from './data/experience.json'
 
 function setColorTheme({ localStorageTheme, systemSettingDark }) {
   if (localStorageTheme !== null) {
@@ -29,6 +30,10 @@ function updateThemeOnHtmlEl({ theme }) {
 
 
 document.addEventListener('DOMContentLoaded', function() {
+
+  /**
+   * Dark/Light theme toggle
+   */
   const button = document.querySelector("[data-theme-toggle]"); 
   const localStorageTheme = localStorage.getItem("theme");
   const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
@@ -47,6 +52,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     currentThemeSetting = newTheme;
   });
+
+  /**
+   * Dynamic skills container
+   */
 
   const skillsContainer = document.getElementById('technologies');
   let sectionList = document.createElement('ul');
@@ -70,5 +79,29 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   skillsContainer.appendChild(sectionList);
+
+  /**
+   * Dynamic work experience
+   */
+
+  const workExpContainer = document.getElementById('work-experience');
+  let workHistory = '';
+  let timeline = document.createElement('div');
+  timeline.className = 'timeline';
+
+  experiences.forEach(exp => {
+    workHistory += `<div class="exp">
+      <div class="day">${exp.date}</div>
+      <div class="work-card">
+        <div class="designation">${exp.position} at ${exp.company}</div>
+        <div class="description">
+          <p>${exp.description}</p>
+        </div>
+      </div>
+    </div>`;
+  });
+
+  timeline.innerHTML = workHistory;
+  workExpContainer.appendChild(timeline);
 });
 
