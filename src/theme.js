124e7dd020cefaf3dba004674f1c159c4d13aff6
Grace Mora <50357533+gizmora@users.noreply.github.com>
2025-01-12 21:48:44 +0800

    chore: seggregate core functions

---
+++ src/theme.js
@@ -0,0 +1,47 @@
+function setColorTheme({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+
+function updateButton({ buttonEl, isDark }) {
+  const icon = isDark ? "&#9728;" : "&#127769;";
+  const label = isDark ? "Change to Light theme" : "Change to Dark theme";
+
+  buttonEl.setAttribute("aria-label", label);
+  buttonEl.innerHTML = icon;
+}
+
+
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+function initializeTheme() {
+  const button = document.querySelector("[data-theme-toggle]");
+  const localStorageTheme = localStorage.getItem("theme");
+  const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+  let currentThemeSetting = setColorTheme({ localStorageTheme, systemSettingDark });
+
+  updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+  updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+  button.addEventListener("click", (event) => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+    localStorage.setItem("theme", newTheme);
+    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+    updateThemeOnHtmlEl({ theme: newTheme });
+
+    currentThemeSetting = newTheme;
+  });
+}
+
+export { initializeTheme };
